1277cd81ba373c0cfe3e822b5b014ee7755ae84d
o0serena0o <57519051+o0serena0o@users.noreply.github.com>
2020-03-08 13:59:14 +0900

    자연수의 합

---
+++ 2/2/2.cpp
@@ -0,0 +1,19 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+int a, b;
+
+int main()
+{
+	cin >> a >> b;
+	int pls = 0;
+	for (int i = a; i <= b; i++)
+	{
+		pls = pls + i;
+		if (i != b)	cout << i << '+';
+		else cout << i << '=';
+	}
+	cout << pls;
+	return 0;
+}
