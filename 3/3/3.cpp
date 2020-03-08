8a4dc673e8545b0a9cd99c289c6bc71327c6a439
o0serena0o <57519051+o0serena0o@users.noreply.github.com>
2020-03-08 14:20:10 +0900

    진약수의 합

---
+++ 3/3/3.cpp
@@ -0,0 +1,21 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int main()
+{
+	cin >> n;
+	int pls = 1;
+	cout << 1;
+	for (int i = 2; i < n; i++)
+	{
+		if (n % i == 0)
+		{
+			pls = pls + i;
+			cout << '+' << i;
+		}
+	}
+	cout << '=' << pls;
+}
