70b65d095f920e2c03abdd7fe3d578c8be6c18ee
o0serena0o <57519051+o0serena0o@users.noreply.github.com>
2020-03-08 13:53:05 +0900

    1부터 n까지의 m의 배수합

---
+++ 1/1/1.cpp
@@ -0,0 +1,19 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, m;
+
+int main()
+{
+	cin >> n >> m;
+	int pls = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		if (i % m == 0)
+		{
+			pls = pls + i;
+		}
+	}
+	cout << pls << endl;
+}
