d2488f90c11248f13ea15fd2af1241cefbe5f147
o0serena0o <57519051+o0serena0o@users.noreply.github.com>
2020-03-08 14:42:01 +0900

    나이차이

---
+++ 4/4/4.cpp
@@ -0,0 +1,21 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int main()
+{
+	cin >> n;
+	int old = 0;
+	int young = 100;
+	for (int i = 0; i < n; i++)
+	{
+		int k;
+		cin >> k;
+		old = max(old, k);
+		young = min(young, k);
+	}
+	int ans = old - young;
+	cout << ans;
+}
