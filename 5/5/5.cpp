9ced11b879ed8aa61c9e3edab516ddd7825dd3b8
o0serena0o <57519051+o0serena0o@users.noreply.github.com>
2020-03-09 11:18:47 +0900

    나이계산 ASCII Tlqkffja

---
+++ 5/5/5.cpp
@@ -0,0 +1,17 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+int year, age;
+
+int main()
+{
+	char num[20];
+	cin >> num;
+	if (num[7] == '1' || num[7] == '2') year = 1900 + ((num[0] - 48) * 10 + (num[1] - 48));
+	else year = 2000 + ((num[0] - 48) * 10 + (num[1] - 48));
+	age = 2019 - year + 1;
+	cout << age;
+	if (num[7] == '1' || num[7] == 3) cout << 'M' << endl;
+	else cout << 'W' << endl;
+	return 0;
+}
